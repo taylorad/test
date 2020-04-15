4f2ede9689aa988fc6b0187aa77e01c91233b58a
taylorad <63725585+taylorad@users.noreply.github.com>
2020-04-15 10:50:47 -0300

    Create Rcapture_script.R

---
+++ Rcapture_script.R
@@ -0,0 +1,17 @@
+require(dplyr)
+require(Rcapture)
+require(FSA)
+
+# mrClosed(MARKED,RECAPTURED,#MARKED IN RECAPS, method=)
+STM_E <- mrClosed(245,248,3, method="Chapman")
+STM_W <- mrClosed(64,65,1, method="Chapman")
+
+mrClosed(245,248,3, method="Bailey")
+
+summary(STM_E, incl.SE=T)
+confint(STM_E, verbose=T)
+
+summary(STM_W, incl.SE=T)
+confint(STM_W, verbose=T)
+
+STM_W$N
